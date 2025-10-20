13210a40735dacf6d30d9ac900f693ca38d88d16
oleg.kishko <oleg.kishko@gmail.com>
2025-10-20 14:44:12 -0400

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 import './RegisterPage.css';
 
@@ -7,9 +10,47 @@ function RegisterPage() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
 
     const handleRegister = async () => {
-        console.log("Register invoked")
+        try {
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            const json = await response.json();
+            
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+                
+                setIsLoggedIn(true);
+
+                navigate('/app');
+            }
+
+            if (json.error) {
+                setShowerr(json.error);
+            }
+
+            return (
+                <div className="text-danger">{showerr}</div>
+            );
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     }
 
     return (
